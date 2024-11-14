2aa1d359a8a9831a2934ed834078eb45913e7eaa
Calvin Hirschler <calvinhirschler@hotmail.com>
2024-11-14 12:59:36 -0600

    Switching to Android SDK. Permissions seem to be granted. Can't get it to render

---
+++ Magic Leap Test/Assets/ImageCaptureAndroid.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.Android;
+
+public class ImageCaptureAndroid : MonoBehaviour
+{
+
+    private void PermissionCallbacksPermissionGranted(string permissionName)
+    {
+        StartCoroutine(DelayedCameraInitialization());
+    }
+
+    private IEnumerator DelayedCameraInitialization()
+    {
+        yield return null;
+        InitializeCamera();
+    }
+
+    private void PermissionCallbacksPermissionDenied(string permissionName)
+    {
+        Debug.LogWarning($"Permission {permissionName} Denied");
+    }
+
+    private void AskCameraPermission()
+    {
+        var callbacks = new PermissionCallbacks();
+        callbacks.PermissionDenied += PermissionCallbacksPermissionDenied;
+        callbacks.PermissionGranted += PermissionCallbacksPermissionGranted;
+        Permission.RequestUserPermission(Permission.Camera, callbacks);
+    }
+
+    void Start()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            AskCameraPermission();
+            return;
+        }
+        InitializeCamera();
+    }
+
+    private void InitializeCamera()
+    {
+        Debug.Log("Camera initialized");
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
